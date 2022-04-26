23e4cfd17e28d1dd3c7a9a4d2410d7228db79cf5
EnLangKat <95473394+EnLangKat@users.noreply.github.com>
2022-04-26 10:37:05 +0200

    Update view_protos.py

--- view_protos.py
+++ view_protos.py
@@ -3,7 +3,7 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 
 num_prototypes = 5
-names = num_protoypes * ["prototype-img"] + num_protoypes * ["prototype-img-original_with_self_act"] + num_protoypes * ["prototype-img-original"]
+names = num_prototypes * ["prototype-img"] + num_prototypes * ["prototype-img-original_with_self_act"] + num_prototypes * ["prototype-img-original"]
 protos = 3 * ["75", "76", "77", "78", "79"] # prototype ids
 name = model_dir + "img/C100P5S0resnet34_E10I5push0.6934/" # location of prototype images
 
