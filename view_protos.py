6b47dd65915c2911da722e1426d5536e8f86dd06
EnLangKat <95473394+EnLangKat@users.noreply.github.com>
2022-04-26 10:34:52 +0200

    Update view_protos.py

--- view_protos.py
+++ view_protos.py
@@ -8,7 +8,7 @@ protos = 3 * ["75", "76", "77", "78", "79"] # prototype ids
 name = model_dir + "img/C100P5S0resnet34_E10I5push0.6934/" # location of prototype images
 
 fig = plt.figure(figsize=(12, 12))
-grid = ImageGrid(fig, 111, nrows_ncols=(3, 5), axes_pad=0.1)
+grid = ImageGrid(fig, 111, nrows_ncols=(3, num_prototypes), axes_pad=0.1)
 
 for ax, i, j in zip(grid, protos, names):
     ax.set_axis_off()
