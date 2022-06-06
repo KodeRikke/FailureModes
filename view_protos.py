874dad85afa7723a490471a133fc8c815e809577
EnLangKat <95473394+EnLangKat@users.noreply.github.com>
2022-06-06 16:29:59 +0200

    Update view_protos.py

--- view_protos.py
+++ view_protos.py
@@ -2,24 +2,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 
-num_prototypes = 10
+num_prototypes = 5
 names = num_prototypes * ["prototype-img"] + num_prototypes * ["prototype-img-original_with_self_act"] + num_prototypes * ["prototype-img-original"]
-#protos = 3 * ["225", "226", "227", "228", "229", "230", "231", "232", "233", "234", "235", "236", "237", "238", "239"] # prototype ids
-#protos = 3 * ["45", "46", "47"]
-protos = 3 * ["150", "151", "152", "153", "154", "155", "156", "157", "158", "159"]
-name = "pushes/img5/C100P10S2E0resnet34/class/" # location of prototype images
+protos = 3 * ['75', '76', '77', '78', '79'] # prototype ids
+name = model_dir + "img/C100P5S3resnet34_E9I19push0.6945/" # location of prototype images 
 
 fig = plt.figure(figsize=(12, 12))
 grid = ImageGrid(fig, 111, nrows_ncols=(3, num_prototypes), axes_pad=0.1)
 
 for ax, i, j in zip(grid, protos, names):
-    ax.set_axis_off()
     im = plt.imread(name + j + i + ".png")
     h, v = np.shape(im)[0], np.shape(im)[1]
     if v != h:
         h = h * 10 / v
     else: 
         h = 10
+    ax.set_axis_off()
     ax.imshow(im, extent = [0, 10, 0, h])
-
 plt.show()
