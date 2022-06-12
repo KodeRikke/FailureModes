a049399fb8aa0d585ab5df6f9905aa92bfedece2
EnLangKat <95473394+EnLangKat@users.noreply.github.com>
2022-06-12 14:16:20 +0200

    Update view_protos.py

--- view_protos.py
+++ view_protos.py
@@ -1,22 +1,19 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
+from PIL import Image
+from os import listdir
 
-num_prototypes = 5
-names = num_prototypes * ["prototype-img"] + num_prototypes * ["prototype-img-original_with_self_act"] + num_prototypes * ["prototype-img-original"]
-protos = 3 * ['75', '76', '77', '78', '79'] # prototype ids
-name = model_dir + "img/C100P5S3resnet34_E9I19push0.6945/" # location of prototype images 
+model_dir = "C:/users/ohrai/desktop/IAI/saved_models/"
+num_prototypes = 15
 
-fig = plt.figure(figsize=(12, 12))
-grid = ImageGrid(fig, 111, nrows_ncols=(3, num_prototypes), axes_pad=0.1)
+files = listdir(model_dir + 'img/C100P15S2resnet34_E9I19push0.7266/')[::-1]
+images = [Image.open(model_dir + 'img/C100P15S2resnet34_E9I19push0.7266/' + f) for f in files]
 
-for ax, i, j in zip(grid, protos, names):
-    im = plt.imread(name + j + i + ".png")
-    h, v = np.shape(im)[0], np.shape(im)[1]
-    if v != h:
-        h = h * 10 / v
-    else: 
-        h = 10
-    ax.set_axis_off()
-    ax.imshow(im, extent = [0, 10, 0, h])
+fig, axes = plt.subplots(nrows=4, ncols=num_prototypes, figsize=(20,20))
+for idx, image in enumerate(images):
+    row = idx // num_prototypes
+    col = idx %  num_prototypes
+    axes[row, col].axis("off")
+    axes[row, col].imshow(image)
+plt.subplots_adjust(wspace=.05, hspace=.05)
 plt.show()
