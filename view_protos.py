b00841fbdc24c72357488962a83e7b3bbe157165
EnLangKat <95473394+EnLangKat@users.noreply.github.com>
2022-06-16 15:12:33 +0200

    Update view_protos.py

--- view_protos.py
+++ view_protos.py
@@ -3,11 +3,14 @@ import numpy as np
 from PIL import Image
 from os import listdir
 
-model_dir = "C:/users/ohrai/desktop/IAI/saved_models/"
-num_prototypes = 15
+model_dir = "./saved_models/"
 
-files = listdir(model_dir + 'img/C100P15S2resnet34_E9I19push0.7266/')[::-1]
-images = [Image.open(model_dir + 'img/C100P15S2resnet34_E9I19push0.7266/' + f) for f in files]
+im_dir = model_dir + 'img/C100P3S0resnet34_e19/'
+
+num_prototypes = 3
+
+files = listdir(im_dir)[::-1]
+images = [Image.open(im_dir + f) for f in files]
 
 fig, axes = plt.subplots(nrows=3, ncols=num_prototypes, figsize=(20,20))
 for idx, image in enumerate(images):
